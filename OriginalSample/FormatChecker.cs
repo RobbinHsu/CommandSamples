c2834907b39c729e165d30ec4a7a40e990d999c0
robbin hsu <robbinhsu@gmail.com>
2020-12-30 13:13:46 +0800

    簡化方法簽章

--- OriginalSample/FormatChecker.cs
+++ OriginalSample/FormatChecker.cs
@@ -22,30 +22,45 @@ namespace OriginalSample
             result.Result = Logic(result.Source);
             return result;
         }
-        public bool Check965(int start,int end,string source) 
+
+        public bool Check965(string source)
         {
-            if (end >= source.Length) return false;
-            int len = end - start + 1 ;
-            return source.Substring(start, len).Contains("965");
+            if (2 >= source.Length) return false;
+            int len = 2 - 0 + 1 ;
+            return source.Substring(0, len).Contains("965");
         }
-        public bool CheckDateTime(int start, int end, string source) 
+
+        public bool CheckFirstDateTime(string source)
         {
-            if (end >= source.Length) return false;
-            int len = end - start + 1;
-            string subString = source.Substring(start, len);
+            if (20 >= source.Length) return false;
+            int len = 20 - 13 + 1;
+            string subString = source.Substring(13, len);
             DateTime d = new DateTime();
             bool ans = DateTime.TryParseExact(subString, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out d);
             if (d == DateTime.MinValue)
                 return false;
             return ans;
         }
+
+        public bool CheckSecondDateTime(string source)
+        {
+            if (28 >= source.Length) return false;
+            int len = 28 - 21 + 1;
+            string subString = source.Substring(21, len);
+            DateTime d = new DateTime();
+            bool ans = DateTime.TryParseExact(subString, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out d);
+            if (d == DateTime.MinValue)
+                return false;
+            return ans;
+        }
+
         public bool Logic(string source) 
         {
             List<bool> result = new List<bool>();
             int count = 0; 
-            result.Add(Check965(0, 2, source));
-            result.Add(CheckDateTime(13, 20, source));
-            result.Add(CheckDateTime(21, 28, source));
+            result.Add(Check965(source));
+            result.Add(CheckFirstDateTime(source));
+            result.Add(CheckSecondDateTime(source));
             foreach (var r in result) 
             {
                 if (r == true) count++;
