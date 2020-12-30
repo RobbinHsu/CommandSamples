f69ef9a2224e076e34b28c9850ee108f88c8409e
robbin hsu <robbinhsu@gmail.com>
2020-12-30 17:01:30 +0800

    Refactor

--- CommandSample001/Invoker.cs
+++ CommandSample001/Invoker.cs
@@ -9,6 +9,7 @@ namespace CommandSample002
     public class Invoker
     {
         protected double _currentValue;
+        private Dictionary<string, Func<double, double>> _dictionary;
 
         public Invoker()
         {
@@ -16,15 +17,13 @@ namespace CommandSample002
 
         public void Compute(string myOperator, double operand)
         {
-            if (myOperator == "+")
+            _dictionary = new Dictionary<string, Func<double, double>>
             {
-                _currentValue = _currentValue + operand;
-            }
+                {"+", (x) => _currentValue + x},
+                {"-", (x) => _currentValue - x},
+            };
 
-            if (myOperator == "-")
-            {
-                _currentValue = _currentValue - operand;
-            }
+            _currentValue = _dictionary[myOperator].Invoke(operand);
 
 
             Console.WriteLine($"{myOperator} {operand} 之後目前的值為 {_currentValue}");
