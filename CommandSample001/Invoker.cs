84363397b7e21e4f205c909ecfbbe124917207ff
robbin hsu <robbinhsu@gmail.com>
2020-12-30 16:24:40 +0800

    建立測試專案

--- CommandSample001/Invoker.cs
+++ CommandSample001/Invoker.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CommandSample002
 {
-    class Invoker
+   public class Invoker
     {
         public Invoker()
         {
---
+++ CommandSample001Tests/InvokerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using CommandSample002;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandSample002.Tests
+{
+    [TestFixture()]
+    public class InvokerTests
+    {
+        [Test()]
+        public void ComputeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
